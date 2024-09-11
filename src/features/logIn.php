964ebb4a8c04129a1f45af294a89aed4f64992d8
0306221184 <0306221184@caothang.edu.vn>
2024-09-11 19:19:07 +0700

    modify deploy

--- src/features/logIn.php
+++ src/features/logIn.php
@@ -13,38 +13,38 @@ if (empty($username) || empty($password)) {
     header('Location: ../../login.php?error=Username and password are required!');
     die();
 }
-try {
-    $queryAuthentication = "SELECT id,email,username, password,role,avatar,gender,phoneNumber FROM user WHERE username='$username'";
-    // Insert user data
-    $userResult = Database::select($queryAuthentication);
-    if ($userResult != false) {
-        $user = $userResult->fetch_assoc();
-        $checkPassword = password_verify($password, $user['password']);
-        if ($checkPassword) {
-            Session::init();
-            Session::set('login', true);
-            Session::set('userId', $user['id']);
-            Session::set('role', $user['role']);
-            Session::set('email', $user['email']);
-            Session::set('username', $user['username']);
-            Session::set('avatar', $user['avatar']);
-            Session::set('gender', $user['gender']);
-            Session::set('phoneNumber', $user['phoneNumber']);
-            header("Location: ../../index.php");
-        } else {
-            header("Location: ../../login.php?error=Wrong password!!");
-            die();
-        }
+// try {
+$queryAuthentication = "SELECT id,email,username, password,role,avatar,gender,phoneNumber FROM user WHERE username='$username'";
+// Insert user data
+$userResult = Database::select($queryAuthentication);
+if ($userResult != false) {
+    $user = $userResult->fetch_assoc();
+    $checkPassword = password_verify($password, $user['password']);
+    if ($checkPassword) {
+        Session::init();
+        Session::set('login', true);
+        Session::set('userId', $user['id']);
+        Session::set('role', $user['role']);
+        Session::set('email', $user['email']);
+        Session::set('username', $user['username']);
+        Session::set('avatar', $user['avatar']);
+        Session::set('gender', $user['gender']);
+        Session::set('phoneNumber', $user['phoneNumber']);
+        header("Location: ../../index.php");
     } else {
-        header("Location: ../../login.php?error=User not exists!!");
+        header("Location: ../../login.php?error=Wrong password!!");
         die();
     }
-} catch (Exception $e) {
-    // Handle database errors
-    $error = $e->getMessage();
-    $econ = Database::$error;
-    echo "system error" . $error;
-    echo "db error" . $econ;
-    // header("Location: ../../login.php?error=$econ!!");
-    // die();
+} else {
+    header("Location: ../../login.php?error=User not exists!!");
+    die();
 }
+// } catch (Exception $e) {
+//     // Handle database errors
+//     $error = $e->getMessage();
+//     $econ = Database::$error;
+//     echo "system error" . $error;
+//     echo "db error" . $econ;
+//     // header("Location: ../../login.php?error=$econ!!");
+//     // die();
+// }
